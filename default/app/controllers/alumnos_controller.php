4ebf9e8eedfe0aeefab73b83a0b81b7b35ec1307
navnelson7 <navnelson7@gmail.com>
2022-03-22 12:01:45 -0600

    Agregando funcion listar usuarios y vista para listar usuarios

--- default/app/controllers/alumnos_controller.php
+++ default/app/controllers/alumnos_controller.php
@@ -1,8 +1,10 @@
 <?php
 
     class AlumnosController extends AppController {
-        public function index($nombres){
-            $this->fecha = date("Y-m-d");
-            $this->nombres = $nombres;
+        public function index(){
+            
+        }
+        public function listarusuarios(){
+            
         }
     }
